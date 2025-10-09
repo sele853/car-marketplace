5ff61e78c767b693a12bde61388103aef9ecac95
saladin <solehadinmoh@gmail.com>
2025-10-09 09:22:38 +0300

    initialize backend server

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log("MongoDB connection error:", error));
+
+app.get('/',(req,res)=>{
+    res.json({message:"it's working"});
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT , () => console.log(`Server running on port ${PORT}`));
